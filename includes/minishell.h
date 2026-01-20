59c3a1cc3fc53c46971bed7a57515f0c39316f46
mperrine <mperrine@student.42angouleme.fr>
2026-01-20 11:18:19 +0100

    add necessary libs and start enum for lexer args

---
+++ includes/minishell.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 10:47:57 by mperrine          #+#    #+#             */
+/*   Updated: 2026/01/20 11:17:37 by mperrine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/time.h>
+# include <sys/resource.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <term.h>
+# include <curses.h>
+
+enum e_lexer {
+	S_QUOTE,
+	D_QUOTE,
+	RED_IN,
+	RED_OUT_REP,
+	RED_OUT_APP,
+	DELIMITER,
+	PIPE,
+	ENV_VAR,
+	COMMAND,
+	CMD_ARG,
+};
+
+#endif
