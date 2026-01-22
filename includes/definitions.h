711b88ec612f79c1046c7b8511a57007aa007406
juperrin <marvin@42.fr>
2026-01-22 13:49:07 +0100

    + created definitions header for typedefs and defines

---
+++ includes/definitions.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   definitions.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 13:47:24 by juperrin          #+#    #+#             */
+/*   Updated: 2026/01/22 13:48:06 by juperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINITIONS_H
+# define DEFINITIONS_H
+
+typedef unsigned int	t_uint;
+typedef unsigned char	t_uint8;
+
+#endif
