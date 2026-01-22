6d647b9cb542e95ca3a726c60dbdc310a144d4b3
juperrin <marvin@42.fr>
2026-01-22 11:15:29 +0100

    + created list header

---
+++ includes/list.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 11:08:39 by juperrin          #+#    #+#             */
+/*   Updated: 2026/01/22 11:13:15 by juperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIST_H
+# define LIST_H
+
+# include <stdlib.h>
+# include "token.h"
+
+typedef struct s_list
+{
+	char	*data;
+	
+}	t_list;
+
+#endif
