f47c2f19120bd5dc0e4520a3424d35be4650bbc6
juperrin <marvin@42.fr>
2026-01-22 13:40:59 +0100

    # updated comms

--- includes/list.h
+++ includes/list.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 11:08:39 by juperrin          #+#    #+#             */
-/*   Updated: 2026/01/22 13:34:13 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/01/22 13:40:46 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,26 @@ typedef struct s_list
 }	t_list;
 
 /**
- * @brief Returns an allocated pointer to a t_list node.
+ * @brief Allocates memory for a new node.
  * @param data The data to store in the node.
  * @param token The corresponding token for data.
+ * @return The created node.
  */
 t_list	*lst_new(char *data, t_token token);
 
 /**
- * @brief Adds a node at the end of head. Returns the new list.
+ * @brief Adds a new node at the end of the list.
  * @param head The head / start of the list.
  * @param data The data to store in the node.
  * @param token The corresponding token for data.
+ * @return The updated version of the list passed in.
  */
 t_list	*lst_add(t_list **head, char *data, t_token token);
 
 /**
  * @brief Removes the last node of head.
  * @param head The head / start of the list.
+ * @return The updated version of the list passed in.
  */
 t_list	*lst_remove(t_list **head);
 
