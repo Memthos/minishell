fe379c8d5ebdef71cd541c007c36ce09c34c318e
mperrine <mperrine@student.42angouleme.fr>
2026-01-22 12:41:04 +0100

    switch token struct name to lowercase

--- includes/token.h
+++ includes/token.h
@@ -6,14 +6,14 @@
 /*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 11:12:03 by juperrin          #+#    #+#             */
-/*   Updated: 2026/01/22 11:27:07 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/01/22 12:40:50 by mperrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKEN_H
 # define TOKEN_H
 
-typedef enum e_TOKENS
+typedef enum e_tokens
 {
 	WORD,
 	ASSIGNMENT_WORD,
