499e7efda8c0e30b6945370072256c2f88120b26
juperrin <marvin@42.fr>
2026-01-22 14:16:02 +0100

    + created return_status enum

--- includes/enums.h
+++ includes/enums.h
@@ -1,17 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   token.h                                            :+:      :+:    :+:   */
+/*   enums.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 11:12:03 by juperrin          #+#    #+#             */
-/*   Updated: 2026/01/22 12:40:50 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/01/22 14:15:18 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKEN_H
 # define TOKEN_H
+
+typedef enum e_return_status
+{
+	SUCCESS,
+	FAILURE,
+}	t_status;
 
 typedef enum e_tokens
 {
