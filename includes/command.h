b1d1cc88544e17f9c22a348e0b4ed349825892af
juperrin <marvin@42.fr>
2026-01-24 09:55:33 +0100

    + added pwd function

--- includes/command.h
+++ includes/command.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 16:13:55 by juperrin          #+#    #+#             */
-/*   Updated: 2026/01/24 01:44:26 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/01/24 09:53:34 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,10 @@ typedef struct s_command
 	t_uint	min_arg;
 	int		(*command)(void *);
 }	t_command;
+
+/**
+ * @brief Displays the current working directory.
+ */
+void	pwd(void);
 
 #endif
