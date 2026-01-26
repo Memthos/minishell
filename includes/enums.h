dd2650110c8560867aa8fb0e183355af726955ec
mperrine <mperrine@student.42angouleme.fr>
2026-01-26 17:24:23 +0100

    accept merge

--- includes/enums.h
+++ includes/enums.h
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   enums.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 11:12:03 by juperrin          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2026/01/26 15:48:12 by mperrine         ###   ########.fr       */
-=======
-/*   Updated: 2026/01/26 14:59:36 by juperrin         ###   ########.fr       */
->>>>>>> 2086ba73a2cd799f2c8b55ac0d29d4e40252d0b1
+/*   Updated: 2026/01/26 17:24:14 by mperrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
