25f712ed2f46de05cb7b8aaf3f081d0a0ed2e800
juperrin <marvin@42.fr>
2026-02-12 11:05:18 +0100

    + created echo command

---
+++ srcs/commands/echo.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/12 10:38:34 by juperrin          #+#    #+#             */
+/*   Updated: 2026/02/12 11:04:37 by juperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+t_status	echo(char *msg, t_uint8 line_break)
+{
+	t_status	code;
+
+	if (NULL == msg)
+	{
+		if (line_break)
+			printf("\n");
+		return (SUCCESS);
+	}
+	code = SUCCESS;
+	if (-1 == write(1, msg, ft_strlen(msg)))
+	{
+		perror("Failed to write on stdout");
+		code = FAILURE;
+	}
+	if (line_break && -1 == write(1, "\n", 1))
+	{
+		perror("Failed to write on stdout");
+		code = FAILURE;
+	}
+	return (code);
+}
