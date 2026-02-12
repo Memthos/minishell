04ad54620a4848a360c7a9b9426b853e7eb76330
juperrin <marvin@42.fr>
2026-02-12 08:06:51 +0100

    Fix nonsens recursion on write error

--- srcs/outputs/error_output.c
+++ srcs/outputs/error_output.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_output.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 10:23:31 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/11 10:07:31 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/02/12 08:06:01 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ t_status	error_output(const char *error)
 		return (FAILURE);
 	if (write(STDERR_FILENO, error, ft_strlen(error)) < 0)
 	{
-		error_output("Failed to write on stderr");
+		printf("Failed to write on stderr");
 		return (FAILURE);
 	}
 	if (write(STDERR_FILENO, ".\n", 2) < 0)
 	{
-		error_output("Failed to write on stderr");
+		printf("Failed to write on stderr");
 		return (FAILURE);
 	}
 	return (SUCCESS);
