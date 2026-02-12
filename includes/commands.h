bc57afec4d75f926a33a0f0a5d5e3c677833c800
juperrin <marvin@42.fr>
2026-02-12 08:04:38 +0100

    + added exec function declaration

--- includes/commands.h
+++ includes/commands.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 16:13:55 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/09 10:29:44 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/12 08:02:19 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 # define COMMANDS_H
 
 # include "definitions.h"
+
+/**
+ * @brief Execute the command line passed in.
+ * @param command The command to execute.
+ * @param input_fd File descriptor to redirect the stdin. -1 to not redirect.
+ * @param output_file File descriptor to redirect the stdout. -1 to not redirect.
+ * @return Nothing on success, the execve error code otherwise.
+ */
+int			exec(char *command, int input_fd, int output_fd);
 
 /**
  * @brief Displays the current working directory.
