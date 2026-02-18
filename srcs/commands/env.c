5afc5ca79cdc543c3607d33f8312070fce576b4d
juperrin <marvin@42.fr>
2026-02-18 09:04:22 +0100

    # env now prints the environments variables

--- srcs/commands/env.c
+++ srcs/commands/env.c
@@ -6,19 +6,20 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 08:25:20 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/18 08:26:48 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/18 09:02:57 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-t_status	penv(void)
+void	env(char *envp[])
 {
-	char	*env;
-
-	env = getenv(NULL);
-	if (NULL == env)
-		return (FAILURE);
-	printf("%s\n", env);
-	return (SUCCESS);	
+	if (NULL == envp)
+		return ;
+	while (*envp)
+	{
+		printf("%s\n", *envp);
+		++envp;
+	}
+	return ;
 }
