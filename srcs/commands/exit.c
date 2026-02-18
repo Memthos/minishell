49a119e4f1f4e4c4ba46c1bb96e906b5464138a3
juperrin <marvin@42.fr>
2026-02-18 10:03:55 +0100

    + created template exit function

---
+++ srcs/commands/exit.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 10:02:10 by juperrin          #+#    #+#             */
+/*   Updated: 2026/02/18 10:03:25 by juperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	exit(void) // Pass here all necessary data that needs to be freed
+{
+	//Free all necessary data
+	return ;
+}
