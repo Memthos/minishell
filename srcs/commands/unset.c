307bd4ead836ae72081060749ace3a14e243631d
juperrin <marvin@42.fr>
2026-02-18 11:53:18 +0100

    # updated unset function

--- srcs/commands/unset.c
+++ srcs/commands/unset.c
@@ -6,31 +6,17 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 09:10:34 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/18 09:55:30 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/18 11:38:26 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-t_status	unset(char *envp[], char *name)
+t_status	unset(t_dictionary **env, char *name)
 {
-	t_status	code;
-
-	if (NULL == envp || NULL == name)
+	if (NULL == env || NULL == *env || NULL == name)
 		return (FAILURE);
-	code = SUCCESS;
-	while (*envp)
-	{
-		if (!strncmp(*envp, name, ft_strlen(name)))
-			break ;
-		++envp;
-	}
-	if (NULL == *envp)
-		code = FAILURE;
-	while (*envp)
-	{
-		*envp = *(envp + 1);
-		++envp;
-	}
-	return (code);
+	if (NULL == dict_remove(env, name))
+		return (FAILURE);
+	return (SUCCESS);
 }
