d659e176192a09a2414f74b79d2f0ab16182866d
juperrin <marvin@42.fr>
2026-02-18 10:21:20 +0100

    # updated exit name

--- srcs/commands/exit.c
+++ srcs/commands/exit.c
@@ -6,13 +6,13 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 10:02:10 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/18 10:03:25 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/18 10:20:59 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	exit(void) // Pass here all necessary data that needs to be freed
+void	ft_exit(void) // Pass here all necessary data that needs to be freed
 {
 	//Free all necessary data
 	return ;
