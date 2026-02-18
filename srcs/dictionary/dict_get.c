5766002223a0b9e99034dc75c78d49d0444fe822
juperrin <marvin@42.fr>
2026-02-18 11:52:34 +0100

    Fix key comparaison because i'm stooopide

--- srcs/dictionary/dict_get.c
+++ srcs/dictionary/dict_get.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 14:06:10 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/17 14:12:19 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/18 11:48:05 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_dictionary	*dict_get(t_dictionary *dict, char *key)
 {
 	if (NULL == dict || NULL == key)
 		return (NULL);
-	while (dict && dict->key != key)
+	while (dict && ft_strcmp(key, dict->key))
 		dict = dict->next;
 	return (dict);
 }
