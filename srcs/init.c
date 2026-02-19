029e08466fa255cf3dafe0ca832e95463c90ac94
juperrin <marvin@42.fr>
2026-02-19 09:17:58 +0100

    + added init_env to init function

--- srcs/init.c
+++ srcs/init.c
@@ -6,14 +6,20 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 02:21:00 by juperrin          #+#    #+#             */
-/*   Updated: 2026/01/24 11:02:18 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/19 09:16:43 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-t_status	init(void)
+t_status	init(char *envp[])
 {
-	init_signals();
+	t_dictionary	*env_dict;
+
+	if (SUCCESS != init_signals())
+		return (FAILURE);
+	env_dict = init_env(envp);
+	if (NULL == env_dict)
+		return (FAILURE);
 	return (SUCCESS);
 }
