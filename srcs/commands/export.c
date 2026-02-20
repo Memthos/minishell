063044efc31b9a9c8226915d46b685be7c6ea735
juperrin <marvin@42.fr>
2026-02-20 18:44:22 +0100

    # updated cmd_export behaviour

--- srcs/commands/export.c
+++ srcs/commands/export.c
@@ -1,34 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/20 13:17:30 by juperrin          #+#    #+#             */
+/*   Updated: 2026/02/20 18:27:53 by juperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/minishell.h"
 
-t_status	export(t_dictionary **env_dict, char *name, char *value, bool concatenate)
+t_status	cmd_export(char **args, t_dictionary **env_dict)
 {
+	t_dictionary	*cpy;
+	bool			concat;
+	char			**entry;
 	char			*tmp;
-	t_dictionary	*entry;
 
-	if (NULL == name && NULL != value)
+	if (NULL == env_dict || NULL == *env_dict)
 		return (FAILURE);
-	if (NULL == name && NULL == value)
+	if (NULL == args || NULL == *args)
 	{
-		env(*env_dict);
-		return (SUCCESS);
+		cpy = *env_dict;
+		dict_sort(&cpy);
+		dict_display(cpy, "=");
+		return (SUCCESS); // Not complete
 	}
-	entry = dict_get(*env_dict, name);
-	if (NULL == entry)
+	while (*args)
 	{
-		if (NULL == dict_add(env_dict, name, value))
-			return (FAILURE);
-		return (SUCCESS);
+		concat = false;
+		entry = split_at(*args, '=');
+		if (NULL == entry)
+		{
+			++args;
+			continue ;
+		}
+		if (0 == ft_strlen(entry[0]) || 0 == ft_strlen(entry[1]))
+		{
+			++args;
+			free(entry[0]);
+			free(entry[1]);
+			free(entry);
+			continue ;
+		}
+		if (entry[0][ft_strlen(entry[0]) - 1] == '+')
+		{
+			concat = true;
+			entry[0][ft_strlen(entry[0]) - 1] = '\0';
+		}
+		if (!check_var_name(entry[0]))
+		{
+			error_output("export : not a valid identifier");
+			++args;
+			free(entry[0]);
+			free(entry[1]);
+			free(entry);
+			continue ;
+		}
+		if (NULL == dict_add(env_dict, entry[0], entry[1]))
+		{
+			if (!concat)
+			{
+				dict_update(*env_dict, entry[0], entry[1]);
+				free(entry[0]);
+			}
+			else
+			{
+				cpy = dict_get(*env_dict, entry[0]);
+				tmp = ft_strjoin((const char *)cpy->data, entry[1]);
+				dict_update(*env_dict, entry[0], tmp);
+				free(entry[0]);
+				free(entry[1]);
+			}
+		}
+		free(entry);
+		++args;
 	}
-	if (concatenate)
-	{
-		tmp = ft_strjoin((char *)entry->data, value);
-		if (NULL == tmp)
-			return (FAILURE);
-		free(entry->data);
-		entry->data = tmp;
-		return (FAILURE);
-	}
-	free(entry->data);
-	entry->data = value;
 	return (SUCCESS);
 }
