bdf7728997af67f39d7a370fd732de75cf2ff440
juperrin <marvin@42.fr>
2026-02-20 08:31:03 +0100

    + added special cases for export

--- srcs/commands/export.c
+++ srcs/commands/export.c
@@ -1,16 +1,21 @@
 #include "../../includes/minishell.h"
 
-t_status	export(t_dictionary **env, char *name, char *value, bool concatenate)
+t_status	export(t_dictionary **env_dict, char *name, char *value, bool concatenate)
 {
 	char			*tmp;
 	t_dictionary	*entry;
 
-	if (NULL == env || NULL == name)
+	if (NULL == name && NULL != value)
 		return (FAILURE);
-	entry = dict_get(*env, name);
+	if (NULL == name && NULL == value)
+	{
+		env(env_dict);
+		return (SUCCESS);
+	}
+	entry = dict_get(*env_dict, name);
 	if (NULL == entry)
 	{
-		if (NULL == dict_add(env, name, value))
+		if (NULL == dict_add(env_dict, name, value))
 			return (FAILURE);
 		return (SUCCESS);
 	}
