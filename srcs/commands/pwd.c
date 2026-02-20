28f25884a4a480be39eced325c30b5eb67f7d831
juperrin <marvin@42.fr>
2026-02-20 17:17:39 +0100

    # updated cmd_pwd function

--- srcs/commands/pwd.c
+++ srcs/commands/pwd.c
@@ -6,26 +6,30 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 09:48:05 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/20 08:35:59 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/20 10:48:39 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-t_status	pwd(void)
+t_status	cmd_pwd(char **args, t_dictionary **env_dict)
 {
-	char	*path;
+	t_dictionary	*PWD;
+	char			*path;
 
+	(void)args;
 	path = getcwd(NULL, 0);
 	if (NULL == path)
 	{
-		path = getenv("PWD");
-		if (NULL == path)
+		PWD = NULL;
+		if (NULL != env_dict)
+			PWD = dict_get(*env_dict, "PWD");
+		if (NULL == PWD)
 		{
-			error_output("cd : Failed to get current directory");
+			error_output("cd : failed to retrieve current working directory");
 			return (FAILURE);
 		}
-		printf("%s\n", path);
+		printf("%s\n", (char *)PWD->data);
 		return (SUCCESS);
 	}
 	printf("%s\n", path);
