b6fe1f59305277c8571c1ba61af858b8df868196
juperrin <marvin@42.fr>
2026-02-23 17:20:56 +0100

    # replaced exit calls with returns

--- srcs/commands/exit.c
+++ srcs/commands/exit.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 10:02:10 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/21 13:19:20 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/23 17:20:12 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,21 @@ t_status	cmd_exit(char **args, t_dictionary **env)
 	(void)env;
 	printf("exit\n");
 	if (NULL == args || NULL == *args)
-		exit(SUCCESS);
+		return (SUCCESS);
 	if (!str_is_digit(*args))
 	{
 		error_output("exit : numeric argument required");
-		exit(BAD_ARG);
+		return (BAD_ARG);
 	}
 	if (NULL != *(args + 1))
 	{
 		error_output("exit : too many arguments");
-		exit(FAILURE);
+		return (FAILURE);
 	}
 	if (SUCCESS != get_number(*args, &code))
 	{
 		error_output("exit : numeric argument required");
-		exit(BAD_ARG);
+		return (BAD_ARG);
 	}
-	exit(code % 256);
+	return (code % 256);
 }
