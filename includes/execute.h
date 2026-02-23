2215e9afacc07941be930546d3568960788245a6
juperrin <marvin@42.fr>
2026-02-23 16:03:50 +0100

    + created execute header for execute function

---
+++ includes/execute.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 14:38:19 by juperrin          #+#    #+#             */
+/*   Updated: 2026/02/23 15:37:46 by juperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECUTE_H
+# define EXECUTE_H
+
+# include "definitions.h"
+# include "lists.h"
+
+t_status	execute(t_shell *shell);
+
+#endif
