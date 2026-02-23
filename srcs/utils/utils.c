824e40ae945139c674b7f2d416aa9d27a1601221
juperrin <marvin@42.fr>
2026-02-23 09:44:08 +0100

    + created string_utils source file to split utils functions in two files

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 11:12:55 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/21 11:44:56 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/23 09:42:57 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,33 +41,6 @@ t_status	get_number(const char *s, int *number)
 	return (SUCCESS);
 }
 
-char	**split_at(const char *str, const char c)
-{
-	char	**split;
-	t_uint	index;
-
-	if (!ft_strchr(str, c))
-		return (NULL);
-	split = (char **)malloc(sizeof(char *) * 2);
-	if (NULL == split)
-		return (NULL);
-	index = 0;
-	while (*(str + index) != c)
-		++index;
-	*split = (char *)malloc(sizeof(char) * (index + 1));
-	*(split + 1) = (char *)malloc(sizeof(char) * (ft_strlen(str) - index));
-	if (NULL == *split || NULL == *(split + 1))
-	{
-		free(*split);
-		free(*(split + 1));
-		free(split);
-		return (NULL);
-	}
-	ft_strlcpy(*split, str, index + 1);
-	ft_strlcpy(*(split + 1), str + index + 1, ft_strlen(str) - index);
-	return (split);
-}
-
 t_uint	count_words(const char *str, const char *set)
 {
 	t_uint	count;
@@ -89,22 +62,6 @@ t_uint	count_words(const char *str, const char *set)
 		++index;
 	}
 	return (count);
-}
-
-void	free_strings(char **strs)
-{
-	t_uint	index;
-
-	if (NULL == strs)
-		return ;
-	index = 0;
-	while (*(strs + index))
-	{
-		free(*(strs + index));
-		++index;
-	}
-	free(strs);
-	return ;
 }
 
 bool	check_var_name(char *name)
@@ -129,17 +86,4 @@ void	swap_str(char **a, char **b)
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
-}
-
-bool	str_is_digit(char *str)
-{
-	if (NULL == str)
-		return (false);
-	while (*str)
-	{
-		if (!ft_isdigit(*str))
-			return (false);
-		++str;
-	}
-	return (true);	
 }
