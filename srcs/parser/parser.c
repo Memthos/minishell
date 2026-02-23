e2d55f4e1c60ebf1edc150783984ad9a97dcad3b
juperrin <marvin@42.fr>
2026-02-23 16:54:25 +0100

    # updated return values with enmu names

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 12:53:01 by mperrine          #+#    #+#             */
-/*   Updated: 2026/02/23 16:23:14 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/23 16:54:08 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static int	parenthesis_check(t_lxr_lst *lxr)
 		if (!lxr->next || lxr->next->token == END_OF_INPUT
 			|| lxr->next->token == R_PAREN || lxr->next->token == AND_IF
 			|| lxr->next->token == OR_IF || lxr->next->token == PIPE)
-			return (1);
+			return (FAILURE);
 	}
 	else if (lxr->token == R_PAREN)
 	{
@@ -28,13 +28,13 @@ static int	parenthesis_check(t_lxr_lst *lxr)
 				|| lxr->next->token == WILDCARD
 				|| lxr->next->token == ASSIGNMENT_W
 				|| lxr->next->token == L_PAREN))
-			return (1);
+			return (FAILURE);
 	}
 	else if ((lxr->token == WORD || lxr->token == ASSIGNMENT_W
 			|| lxr->token == WILDCARD) && lxr->next
 		&& lxr->next->token == L_PAREN)
-		return (1);
-	return (0);
+		return (FAILURE);
+	return (SUCCESS);
 }
 
 static int	quote_check(t_lxr_lst *lxr)
@@ -43,7 +43,7 @@ static int	quote_check(t_lxr_lst *lxr)
 	t_quote_t	quote_state;
 
 	if (!lxr->data)
-		return (0);
+		return (SUCCESS);
 	i = 0;
 	quote_state = 0;
 	while (lxr->data[i])
@@ -52,27 +52,27 @@ static int	quote_check(t_lxr_lst *lxr)
 		i++;
 	}
 	if (quote_state != 0)
-		return (1);
-	return (0);
+		return (FAILURE);
+	return (SUCCESS);
 }
 
 static int	checker_lxr(t_lxr_lst *lxr)
 {
 	if (!lxr)
-		return (1);
+		return (FAILURE);
 	if (lxr->token == AND_IF || lxr->token == OR_IF || lxr->token == PIPE)
-		return (1);
+		return (FAILURE);
 	while (lxr->token != END_OF_INPUT)
 	{
 		if (lxr->p_dpt < 0 || parenthesis_check(lxr))
-			return (1);
+			return (FAILURE);
 		if (quote_check(lxr))
-			return (1);
+			return (FAILURE);
 		lxr = lxr->next;
 	}
 	if (lxr->p_dpt != 0)
-		return (1);
-	return (0);
+		return (FAILURE);
+	return (SUCCESS);
 }
 
 int	parser(char *s, t_shell *shell)
@@ -83,21 +83,21 @@ int	parser(char *s, t_shell *shell)
 	if (lexer(&lxr, s))
 	{
 		lxr_lst_clear(&lxr);
-		return (1);
+		return (FAILURE);
 	}
 	set_final_tokens(lxr);
 	if (checker_lxr(lxr))
 	{
 		lxr_lst_clear(&lxr);
-		return (1);
+		return (FAILURE);
 	}
 	shell->cmd_ast = complete_command_r(&lxr);
 	lxr_lst_clear(&lxr);
 	if (!shell->cmd_ast)
-		return (1);
+		return (FAILURE);
 	if (expand(shell->cmd_ast, shell->env))
-		return (1);
+		return (FAILURE);
 	if (update_quotes(shell->cmd_ast))
-		return (1);
-	return (0);
+		return (FAILURE);
+	return (SUCCESS);
 }
