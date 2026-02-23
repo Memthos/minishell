ebe78bafca8130127256a0be6809c9edca80b479
juperrin <marvin@42.fr>
2026-02-23 16:04:39 +0100

    + added ast to shell structure

--- includes/shell.h
+++ includes/shell.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 10:48:47 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/23 11:13:04 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/23 15:46:20 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 # define SHELL_H
 
 # include "dictionary.h"
+# include "lists.h"
 # include "commands.h"
 
 typedef struct s_shell
 {
 	t_dictionary	*env;
 	const t_command	**commands;
+	t_ast_lst		*cmd_ast;
 }	t_shell;
 
 #endif
