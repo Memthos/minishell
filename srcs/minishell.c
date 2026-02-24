1fd13ab649f95d68b77305390e2cf4839ad184e2
juperrin <marvin@42.fr>
2026-02-24 10:57:27 +0100

    + added minishell function that contains main loop

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,11 +6,32 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 10:52:18 by mperrine          #+#    #+#             */
-/*   Updated: 2026/02/23 16:20:49 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/24 10:49:11 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+static t_status	minishell(t_shell *shell)
+{
+	char	*line;
+
+	if (NULL == shell)
+		return (SUCCESS);
+	while (true)
+	{
+		line = readline("$>");
+		if (SUCCESS != parser(line, shell))
+		{
+			ast_lst_clear(&shell->cmd_ast);
+			error_output("Error append while parsing the command\n");
+			continue ;
+		}
+		if (SUCCESS != execute(shell))
+			error_output("Error append while executing the command\n");
+	}
+	return (SUCCESS);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -21,6 +42,7 @@ int	main(int argc, char **argv, char **envp)
 	shell = init(envp);
 	if (NULL == shell)
 		return (FAILURE);
+	minishell(shell);
 	destroy(shell);
 	return (SUCCESS);
 }
