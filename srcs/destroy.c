9b0448a522b601346240604e9294e8f2df156a4f
juperrin <marvin@42.fr>
2026-02-24 10:56:59 +0100

    # norminetted

--- srcs/destroy.c
+++ srcs/destroy.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 16:07:34 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/23 16:18:48 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/24 10:50:00 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ void	destroy(t_shell *shell)
 	dict_clear(&shell->env);
 	ast_lst_clear(&shell->cmd_ast);
 	free(shell);
-	return ;	
+	return ;
 }
