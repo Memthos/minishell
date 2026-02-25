e87fa306bc875a7cd7fbe3d403c022496e829628
juperrin <marvin@42.fr>
2026-02-25 15:15:41 +0100

    + added locals field for local variables

--- includes/shell.h
+++ includes/shell.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 10:48:47 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/24 16:13:37 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/25 15:11:09 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 typedef struct s_shell
 {
 	t_dictionary	*env;
+	t_dictionary	*locals;
 	t_ast_lst		*cmd_ast;
 }	t_shell;
 
