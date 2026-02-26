1664e0d8011e1de997a8fd932c7e1c057dc3412c
juperrin <marvin@42.fr>
2026-02-26 09:51:52 +0100

    + added dict_clear call for shell locals

--- srcs/destroy.c
+++ srcs/destroy.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 16:07:34 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/24 10:50:00 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/26 09:51:31 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	destroy(t_shell *shell)
 	if (NULL == shell)
 		return ;
 	dict_clear(&shell->env);
+	dict_clear(&shell->locals);
 	ast_lst_clear(&shell->cmd_ast);
 	free(shell);
 	return ;
