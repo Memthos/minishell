dd18c38f7a0021ce9369ffe5d2487584f1eecfbb
juperrin <marvin@42.fr>
2026-02-26 14:21:50 +0100

    + added ALLOCATION_ERROR value in return_status enum

--- includes/definitions.h
+++ includes/definitions.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 13:47:24 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/26 09:15:39 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/26 11:14:57 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ typedef enum e_return_status
 	SUCCESS,
 	FAILURE,
 	BAD_ARG,
+	ALLOCATION_FAILURE,
 	ASSIGN_FAILURE,
 	PIPE_FAILURE,
 	FORK_FAILURE,
