dadc9ae64a542ac38b9f3609d2c476ffc028f77a
juperrin <marvin@42.fr>
2026-02-27 09:32:26 +0100

    + added close calls for stdin and stdout duplicated fd

--- srcs/destroy.c
+++ srcs/destroy.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 16:07:34 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/26 09:51:31 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/27 09:30:06 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	destroy(t_shell *shell)
 	dict_clear(&shell->env);
 	dict_clear(&shell->locals);
 	ast_lst_clear(&shell->cmd_ast);
+	close(shell->stdin_fd);
+	close(shell->stdout_fd);
 	free(shell);
 	return ;
 }
