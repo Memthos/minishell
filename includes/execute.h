2dd8f239fc28b123f0264a28f221d7d367360967
juperrin <marvin@42.fr>
2026-03-03 11:17:42 +0100

    # updated execute function prototype

--- includes/execute.h
+++ includes/execute.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 14:38:19 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/23 15:37:46 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/02/26 08:20:59 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include "definitions.h"
 # include "lists.h"
 
-t_status	execute(t_shell *shell);
+t_status	execute(t_ast_lst *cmd, t_shell *shell);
 
 #endif
