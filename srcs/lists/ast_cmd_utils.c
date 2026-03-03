65dbba9fced0c471419b59f849722665230b8616
juperrin <marvin@42.fr>
2026-03-03 11:03:41 +0100

    + added ast_pipe_count function definition

--- srcs/lists/ast_cmd_utils.c
+++ srcs/lists/ast_cmd_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ast_cmd_size.c                                     :+:      :+:    :+:   */
+/*   ast_cmd_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 14:48:22 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/23 15:56:28 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/02 13:39:07 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,18 @@ t_uint	ast_cmd_size(t_ast_lst *ast)
 		++size;
 	}
 	return (size);
+}
+
+t_uint	ast_pipe_count(t_ast_lst *ast)
+{
+	t_uint	count;
+
+	if (NULL == ast)
+		return (0);
+	count = 0;
+	if (PIPE == ast->token)
+		count = 1;
+	count += ast_pipe_count(ast->left);
+	count += ast_pipe_count(ast->right);
+	return (count);	
 }
