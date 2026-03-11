a1b0c616dc679dc2786fef4d017afcfbf1e544e2
juperrin <marvin@42.fr>
2026-03-11 10:44:11 +0100

    + added shell->cur_cmd free

--- srcs/destroy.c
+++ srcs/destroy.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 16:07:34 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/05 10:28:43 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/11 10:21:12 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@ void	destroy(t_shell *shell)
 {
 	if (NULL == shell)
 		return ;
+	if (NULL != shell->cur_cmd)
+		free(shell->cur_cmd);
 	dict_clear(&shell->env);
 	dict_clear(&shell->locals);
 	ast_lst_clear(&shell->cmd_ast);
-	close(shell->pipe_logic.stdin_fd);
-	close(shell->pipe_logic.stdout_fd);
 	free(shell);
+	printf("destroyed shell\n");
 	return ;
 }
