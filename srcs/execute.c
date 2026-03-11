e77a2f6c864479d27bdb8521950cfeb071c10f7b
juperrin <marvin@42.fr>
2026-03-11 09:32:33 +0100

    Let's restart from scratch

--- srcs/execute.c
+++ srcs/execute.c
@@ -6,13 +6,15 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 10:54:56 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/25 15:11:30 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/11 09:08:14 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-t_status	execute(t_shell *shell)
+t_status	execute(t_ast_lst *cmd, t_shell *shell)
 {
-	return (SUCCESS);
+	if (NULL == cmd)
+		return (SUCCESS);
+	return (shell->exitno);
 }
