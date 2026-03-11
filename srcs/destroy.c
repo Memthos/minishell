6f1a64cba05d1348c79f87efb1324eab19d1d30f
juperrin <marvin@42.fr>
2026-03-11 11:18:24 +0100

    + added pids list free in destroy func

--- srcs/destroy.c
+++ srcs/destroy.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 16:07:34 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/11 10:21:12 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/11 11:17:43 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	destroy(t_shell *shell)
 		return ;
 	if (NULL != shell->cur_cmd)
 		free(shell->cur_cmd);
+	if (NULL != shell->pids)
+		free(shell->pids);
 	dict_clear(&shell->env);
 	dict_clear(&shell->locals);
 	ast_lst_clear(&shell->cmd_ast);
