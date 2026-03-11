26141740e6545e580cbed545ada03cd061c91f8b
juperrin <marvin@42.fr>
2026-03-11 16:43:43 +0100

    Fix invalid read because of use after free

--- srcs/commands/exit.c
+++ srcs/commands/exit.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 10:02:10 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/24 16:24:39 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/11 16:42:38 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,29 @@ t_status	cmd_exit(char **args, t_shell *shell)
 	int	code;
 
 	printf("exit\n");
-	destroy(shell);
 	if (NULL == args[1])
+	{
+		destroy(shell);
 		exit(SUCCESS);
+	}
 	if (!str_is_digit(args[1]))
 	{
 		error_output("exit : numeric argument required");
+		destroy(shell);
 		exit(BAD_ARG);
 	}
 	if (NULL != args[2])
 	{
 		error_output("exit : too many arguments");
+		destroy(shell);
 		exit(FAILURE);
 	}
 	if (SUCCESS != get_number(args[1], &code))
 	{
 		error_output("exit : numeric argument required");
+		destroy(shell);
 		exit(BAD_ARG);
 	}
+	destroy(shell);
 	exit(code % 256);
 }
