36fb42739453f658a1e5c5f973f1c6b76f1cfe40
juperrin <marvin@42.fr>
2026-03-13 15:02:19 +0100

    + added suffix string to print in dict_display function

--- srcs/dictionary/dict_display.c
+++ srcs/dictionary/dict_display.c
@@ -6,17 +6,17 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 11:43:24 by juperrin          #+#    #+#             */
-/*   Updated: 2026/02/23 09:38:27 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/13 14:35:00 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	dict_display(t_dictionary *dict, char *prefix, char *sep)
+void	dict_display(t_dictionary *dict, char *prefix, char *sep, char *suffix)
 {
 	while (dict)
 	{
-		printf("%s%s%s%s\n", prefix, dict->key, sep, (char *)dict->data);
+		printf("%s%s%s%s%s\n", prefix, dict->key, sep, (char *)dict->data, suffix);
 		dict = dict->next;
 	}
 	return ;
