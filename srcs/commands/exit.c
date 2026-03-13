829e7ec82be1df3d86165cbd962cd63e45a6e1d6
juperrin <marvin@42.fr>
2026-03-13 11:06:26 +0100

    # updated destroy_shell function call + changed output fd for 'exit' message

--- srcs/commands/exit.c
+++ srcs/commands/exit.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 10:02:10 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/11 16:42:38 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/13 11:03:00 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,30 @@ t_status	cmd_exit(char **args, t_shell *shell)
 {
 	int	code;
 
-	printf("exit\n");
+	error_output("exit");
 	if (NULL == args[1])
 	{
-		destroy(shell);
+		destroy_shell(shell);
 		exit(SUCCESS);
 	}
 	if (!str_is_digit(args[1]))
 	{
 		error_output("exit : numeric argument required");
-		destroy(shell);
+		destroy_shell(shell);
 		exit(BAD_ARG);
 	}
 	if (NULL != args[2])
 	{
 		error_output("exit : too many arguments");
-		destroy(shell);
+		destroy_shell(shell);
 		exit(FAILURE);
 	}
 	if (SUCCESS != get_number(args[1], &code))
 	{
 		error_output("exit : numeric argument required");
-		destroy(shell);
+		destroy_shell(shell);
 		exit(BAD_ARG);
 	}
-	destroy(shell);
+	destroy_shell(shell);
 	exit(code % 256);
 }
