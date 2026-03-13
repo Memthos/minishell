d56a7f012422dc9317e273a5ce21e43a7ab7ab10
juperrin <marvin@42.fr>
2026-03-13 14:27:32 +0100

    + added ft_close calls in destroy shell function + created wait_for_processes function + added update_pids function

--- srcs/shell.c
+++ srcs/shell.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 10:47:21 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/13 10:52:53 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/13 13:53:59 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,39 @@ t_status	update_pids(t_shell *shell, pid_t pid)
 {
 	if (NULL == shell || pid < 1)
 		return (FAILURE);
-	shell->pids = increment_array(shell->pids, shell->pid_count, sizeof(pid_t));
-	if (NULL == shell->pids)
+	shell->pids.pids = increment_array(shell->pids.pids,
+			shell->pids.pid_count, sizeof(pid_t));
+	if (NULL == shell->pids.pids)
 	{
 		perror("malloc");
 		shell->exitno = ALLOCATION_FAILURE;
 		return (ALLOCATION_FAILURE);
 	}
-	shell->pids[shell->pid_count] = pid;
-	++shell->pid_count;
+	shell->pids.pids[shell->pids.pid_count] = pid;
+	++shell->pids.pid_count;
 	return (SUCCESS);
+}
+
+t_status	wait_for_processes(t_shell *shell)
+{
+	if (NULL == shell)
+		return (FAILURE);
+	if (NULL == shell->pids.pids)
+		return (SUCCESS);
+	shell->pids.pid_index = 0;
+	while (shell->pids.pid_index < shell->pids.pid_count)
+	{
+		waitpid(shell->pids.pids[shell->pids.pid_index],
+			(int *)&shell->exitno, 0);
+		if (WIFEXITED(shell->exitno))
+			shell->exitno = WEXITSTATUS(shell->exitno);
+		dprintf(2, "Process %d exited with status %d\n", shell->pids.pids[shell->pids.pid_index], shell->exitno);
+		++shell->pids.pid_index;
+	}
+	free(shell->pids.pids);
+	shell->pids.pids = NULL;
+	shell->pids.pid_count = 0;
+	return (shell->exitno);
 }
 
 void	destroy_shell(t_shell *shell)
@@ -34,12 +57,12 @@ void	destroy_shell(t_shell *shell)
 		return ;
 	if (NULL != shell->cur_cmd)
 		free(shell->cur_cmd);
-	if (NULL != shell->pids)
-		free(shell->pids);
-	ft_close(&shell->pipe1[0]);
-	ft_close(&shell->pipe1[1]);
-	ft_close(&shell->pipe2[0]);
-	ft_close(&shell->pipe2[1]);
+	if (NULL != shell->pids.pids)
+		free(shell->pids.pids);
+	ft_close(&shell->pipes.pipe1[0]);
+	ft_close(&shell->pipes.pipe1[1]);
+	ft_close(&shell->pipes.pipe2[0]);
+	ft_close(&shell->pipes.pipe2[1]);
 	dict_clear(&shell->env);
 	dict_clear(&shell->locals);
 	ast_lst_clear(&shell->cmd_ast);
