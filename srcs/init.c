6d870fb23fc57fc64d63f431d56afcb2f9bf117c
juperrin <marvin@42.fr>
2026-03-13 14:25:35 +0100

    + created set_shell function to split init function

--- srcs/init.c
+++ srcs/init.c
@@ -6,11 +6,33 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 02:21:00 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/12 15:20:15 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/13 14:25:10 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+static void	set_shell(t_shell *shell)
+{
+	if (NULL == shell)
+		return ;
+	shell->cmd_ast = NULL;
+	shell->locals = NULL;
+	shell->cur_cmd = NULL;
+	shell->exitno = SUCCESS;
+	shell->pipes.redirect_output = false;
+	shell->pipes.redirect_input = false;
+	shell->pipes.pipe1[0] = -1;
+	shell->pipes.pipe1[1] = -1;
+	shell->pipes.pipe2[0] = -1;
+	shell->pipes.pipe2[1] = -1;
+	shell->pipes.pipe_index = 0;
+	shell->pipes.pipe_depth = 0;
+	shell->pids.pids = NULL;
+	shell->pids.pid_count = 0;
+	shell->pids.pid_index = 0;
+	return ;
+}
 
 t_shell	*init(char *envp[])
 {
@@ -30,20 +52,6 @@ t_shell	*init(char *envp[])
 		free(shell);
 		return (NULL);
 	}
-	shell->cmd_ast = NULL;
-	shell->locals = NULL;
-	shell->cur_cmd = NULL;
-	shell->exitno = SUCCESS;
-	shell->redirect_output = false;
-	shell->redirect_input = false;
-	shell->pipe1[0] = -1;
-	shell->pipe1[1] = -1;
-	shell->pipe2[0] = -1;
-	shell->pipe2[1] = -1;
-	shell->pipe_index = 0;
-	shell->pipe_depth = 0;
-	shell->pids = NULL;
-	shell->pid_count = 0;
-	shell->pid_index = 0;
+	set_shell(shell);
 	return (shell);
 }
