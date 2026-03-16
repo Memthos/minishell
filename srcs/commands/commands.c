2193118487325802a335e299522b40fc94001c5e
juperrin <marvin@42.fr>
2026-03-16 13:23:43 +0100

    + added check to print dup2 error in input redirect

--- srcs/commands/commands.c
+++ srcs/commands/commands.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 14:37:34 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/16 13:12:25 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/16 13:22:42 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -162,7 +162,8 @@ t_status	run_comand(t_shell *shell)
 		{
 			if (-1 == dup2(shell->redirects.input_redirect_fd, STDIN_FILENO))
 			{
-				perror("dup2");
+				if (-1 != shell->redirects.input_redirect_fd)
+					perror("dup2");
 				destroy_shell(shell);
 				exit(DUP_FAILURE);
 			}
