e62bf236fba07b477fa8161b37c3a5c2d5d79cb9
juperrin <marvin@42.fr>
2026-03-17 14:11:16 +0100

    + added /bin/ auto add if not in the command line

--- srcs/commands/exec.c
+++ srcs/commands/exec.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 08:48:38 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/16 16:18:44 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/17 14:03:04 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,36 @@
 
 t_status	cmd_exec(char **args, t_shell *shell)
 {
+	char	*save;
+	char	*tmp;
 	char	**envp;
 
 	if (NULL == args || NULL == *args)
 		return (FAILURE);
+	save = *args;
 	envp = dict_to_array(shell->env, '=');
+	if ('/' != **args && dict_get(shell->env, "PATH"))
+	{
+		tmp = ft_strjoin("/bin/", *args);
+		if (NULL == tmp)
+			perror("malloc");
+		else
+			*args = tmp;
+	}
 	execve(*args, args, envp);
 	if (ENOENT == errno)
 	{
-		if (ft_strchr(*args, '/'))
-			perror(*args);
+		if (ft_strchr(*args, '/') || NULL == dict_get(shell->env, "PATH"))
+			perror(save);
 		else
 		{
-			error_output(*args);
+			error_output(save);
 			error_output(": command not found\n");
 		}
 	}
+	if (*args != save)
+		free(*args);
+	*args = save;
 	free_strings(envp);
 	return (127);
 }
