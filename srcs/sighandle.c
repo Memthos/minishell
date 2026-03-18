23d3a7889569598be64322e5faeed92770fdbcdf
juperrin <marvin@42.fr>
2026-03-18 13:22:17 +0100

    + added shell last_exitno update in sig_intercept func

--- srcs/sighandle.c
+++ srcs/sighandle.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 00:46:08 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/17 18:37:34 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/18 13:20:19 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,39 @@ int	g_signal = 0;
 
 static void	sig_intercept(int signo, siginfo_t *info, void *other)
 {
+	t_shell	*shell;
+
 	(void)info;
-	(void)other;
 	g_signal = signo;
-	if (SIGINT == g_signal)
+	shell = (t_shell *)other;
+	if (shell)
+		shell->last_exitno = 128 + SIGINT;
+	if (SIGINT == signo)
 	{
 		write(STDERR_FILENO, "\n", 1);
 		rl_replace_line("", 1);
 		rl_on_new_line();
 		rl_redisplay();
+		return ;
+	}
+	if (SIGQUIT == signo)
+	{
+		printf("SIGQUIT\n");
+		return ;
 	}
 	return ;
 }
 
-t_status	init_signals(void)
+t_status	init_signals(t_shell *shell)
 {
 	struct sigaction	action;
 
 	ft_bzero(&action, sizeof(action));
 	action.sa_sigaction = &sig_intercept;
 	action.sa_flags = SA_SIGINFO;
-	if (SUCCESS != sigaction(SIGINT, &action, NULL))
+	if (SUCCESS != sigaction(SIGINT, &action, shell))
 		return (FAILURE);
-	if (SUCCESS != sigaction(SIGQUIT, &action, NULL))
+	if (SUCCESS != sigaction(SIGQUIT, &action, shell))
 		return (FAILURE);
 	return (SUCCESS);
 }
