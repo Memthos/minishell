bcb04658eaa3be22873fccb35dfb72204b424a37
juperrin <marvin@42.fr>
2026-03-23 17:25:33 +0100

    feat : prints 'Quit\n' if received sig is SIGQUIT

--- srcs/shell.c
+++ srcs/shell.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 10:47:21 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/23 17:17:36 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/03/23 17:24:20 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,8 @@ t_status	wait_for_processes(t_shell *shell)
 			shell->exitno = WTERMSIG(shell->exitno) + 128;
 		++shell->pids.pid_index;
 	}
+	if (SIGQUIT + 128 == shell->exitno)
+		printf("Quit\n");
 	free(shell->pids.pids);
 	shell->pids.pids = NULL;
 	shell->pids.pid_count = 0;
