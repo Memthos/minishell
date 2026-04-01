17fd09021ea0af1fed88d6d790ee90296799fbc8
juperrin <marvin@42.fr>
2026-04-01 14:03:06 +0200

    + added get_cur_pipe func declaration

--- includes/shell.h
+++ includes/shell.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 10:48:47 by juperrin          #+#    #+#             */
-/*   Updated: 2026/04/01 13:31:56 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/01 13:39:15 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,12 @@ typedef struct s_shell
  * @brief Initialises pipe_logic struct values to defaults.
  */
 t_status	init_pipes(t_pipe_logic *pipes);
+
+/**
+ * @brief Returns the current pipe / cmp_pipe where outputs
+ * needs to be redirected, based on pipe_index / cmp_pipe_index.
+ */
+int	*get_cur_pipe(t_pipe_logic *pipes, bool cmp_pipe);
 
 /**
  * @brief Adds pid to the list of pids in shell.
