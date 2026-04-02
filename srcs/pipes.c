354513b8b7cabd50897c2923c58c03f323f57979
juperrin <marvin@42.fr>
2026-04-02 14:54:08 +0200

    + added missing pipe_logic fields initialisation in init_pipes func

--- srcs/pipes.c
+++ srcs/pipes.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 14:13:45 by juperrin          #+#    #+#             */
-/*   Updated: 2026/04/01 15:32:33 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/02 14:53:03 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ t_status	init_pipes(t_pipe_logic *pipes)
 		return (FAILURE);
 	pipes->redirect_input = false;
 	pipes->redirect_output = false;
+	pipes->cmp_redirect = false;
 	pipes->left_pipe[0] = -1;
 	pipes->left_pipe[1] = -1;
 	pipes->left_cmp_pipe[0] = -1;
@@ -27,6 +28,7 @@ t_status	init_pipes(t_pipe_logic *pipes)
 	pipes->right_cmp_pipe[0] = -1;
 	pipes->right_cmp_pipe[1] = -1;
 	pipes->pipe_index = 0;
+	pipes->cmp_pipe_index = 0;
 	pipes->pipe_depth = 0;
 	return (SUCCESS);
 }
