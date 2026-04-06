14923781f2a1e59c6b8d9529de3e7b66664803b1
juperrin <marvin@42.fr>
2026-04-06 17:45:16 +0200

    feat : env now prints itself environment variables and does not display one that have not data

--- srcs/commands/env.c
+++ srcs/commands/env.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 12:51:11 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/24 10:35:35 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/06 17:19:49 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 
 t_status	cmd_env(char **args, t_shell *shell)
 {
+	t_dictionary	*cpy;
+
 	if (NULL == shell)
 		return (FAILURE);
 	if (NULL != args[1])
 		return (FAILURE);
-	dict_display(shell->env, "", "=", "");
+	cpy = shell->env;
+	while (cpy)
+	{
+		if (NULL != cpy->data)
+			printf("%s=%s\n", cpy->key, (char *)cpy->data);
+		cpy = cpy->next;
+	}
 	return (SUCCESS);
 }
