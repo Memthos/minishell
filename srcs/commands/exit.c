cda471443680944a6054646b232cce89dc2ce8fc
juperrin <marvin@42.fr>
2026-04-07 14:23:29 +0200

    feat : improved error_output error arg

--- srcs/commands/exit.c
+++ srcs/commands/exit.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 10:02:10 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/19 14:29:45 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/07 14:22:53 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_status	cmd_exit(char **args, t_shell *shell)
 	int	code;
 
 	if (!shell->pipes.pipe_depth)
-		error_output("exit", -1);
+		error_output("exit", NO_ERR_MSG);
 	if (NULL == args[1])
 	{
 		destroy_shell(shell);
@@ -25,13 +25,13 @@ t_status	cmd_exit(char **args, t_shell *shell)
 	}
 	if (NULL != args[2])
 	{
-		error_output("exit : too many arguments", -1);
+		error_output("exit", TOO_MUCH_ARG);
 		destroy_shell(shell);
 		exit(FAILURE);
 	}
 	if (SUCCESS != get_number(args[1], &code))
 	{
-		error_output("exit : numeric argument required", -1);
+		error_output("exit : numeric argument required", NO_ERR_MSG);
 		destroy_shell(shell);
 		exit(BAD_ARG);
 	}
