3e7ab8942431628bd766f1d02766c5a6f36d4748
Memthos <perrinetrebi.m@gmail.com>
2026-04-08 16:54:43 +0200

    remove io number

--- srcs/parser/lxr_post_tokenization.c
+++ srcs/parser/lxr_post_tokenization.c
@@ -6,7 +6,7 @@
 /*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 22:31:18 by mperrine          #+#    #+#             */
-/*   Updated: 2026/04/03 16:08:01 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/04/08 14:27:39 by mperrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,20 +27,6 @@ static void	set_wildcard(t_lxr_lst *lxr)
 	}
 }
 
-static void	set_io_number(t_lxr_lst *lxr)
-{
-	size_t	i;
-
-	if (!lxr->data)
-		return ;
-	i = 0;
-	while (lxr->data[i] && lxr->data[i] >= '0' && lxr->data[i] <= '9')
-		i++;
-	if (i == ft_strlen(lxr->data) && lxr->next
-		&& (lxr->next->token > 4 && lxr->next->token < 9))
-		lxr->token = IO_NUMBER;
-}
-
 void	set_final_tokens(t_lxr_lst **lxr, t_status *status)
 {
 	t_lxr_lst	**cpy;
@@ -51,7 +37,6 @@ void	set_final_tokens(t_lxr_lst **lxr, t_status *status)
 	while (*cpy)
 	{
 		set_wildcard(*cpy);
-		set_io_number(*cpy);
 		if ((*cpy)->token == TOKEN)
 			lxr_lst_pop(cpy);
 		else
