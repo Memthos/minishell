f85cb03f43fda320079179d20ec6ef4299de9f4f
juperrin <marvin@42.fr>
2026-04-09 15:03:53 +0200

    + added stack_lst header to minishell header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 10:47:57 by mperrine          #+#    #+#             */
-/*   Updated: 2026/04/07 14:26:56 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/09 14:35:40 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,9 @@
 
 //Dictionary header
 # include "dictionary.h"
+
+//Stack header
+# include "stack_lst.h"
 
 # include "execute.h"
 
