5e4b073fa2f4a2addf3a60c2367df0b48ea3d79d
juperrin <marvin@42.fr>
2026-04-09 17:04:03 +0200

    feat : created pipe_get function

--- includes/pipe.h
+++ includes/pipe.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 15:15:39 by juperrin          #+#    #+#             */
-/*   Updated: 2026/04/09 15:41:12 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/09 17:03:09 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,5 @@ typedef struct s_pipe
 t_pipe	*pipe_new(void);
 
 void	pipe_close(t_pipe *pipe);
+
+t_pipe	*pipe_get(t_shell *shell, t_side side);
--- srcs/pipe.c
+++ srcs/pipe.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 15:18:11 by juperrin          #+#    #+#             */
-/*   Updated: 2026/04/09 15:39:38 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/09 17:02:07 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,24 @@ t_pipe	*pipe_new(void)
 		return (NULL);
 	}
 	return (new);
+}
+
+t_pipe	*pipe_get(t_shell *shell, t_side side)
+{
+	t_stack	*cpy;
+	t_pipe	*last;
+
+	if (NULL == shell)
+		return (NULL);
+	last = NULL;
+	cpy = shell->pipe_stack;
+	while (cpy)
+	{
+		if (((t_pipe *)cpy->data)->side == side)
+			last = (t_pipe *)cpy->data;
+		cpy = cpy->next;
+	}
+	return (last);
 }
 
 void	pipe_close(t_pipe *pipe)
