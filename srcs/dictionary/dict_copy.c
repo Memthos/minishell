0d28bb9b780d23e7d3d5332accb55d2761798702
juperrin <marvin@42.fr>
2026-04-10 14:20:14 +0200

    fix : added check in case of malloc failure in dict_cpy func

--- srcs/dictionary/dict_copy.c
+++ srcs/dictionary/dict_copy.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 15:01:29 by juperrin          #+#    #+#             */
-/*   Updated: 2026/03/13 15:25:01 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/10 14:19:50 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,12 @@ t_dictionary	*dict_copy(const t_dictionary *dict)
 	while (dict->next)
 	{
 		cpy->next = malloc(sizeof(t_dictionary));
+		if (NULL == cpy->next)
+		{
+			perror("malloc");
+			dict_clear(&cpy);
+			return (NULL);
+		}
 		cpy->next->key = ft_strdup(dict->next->key);
 		cpy->next->data = ft_strdup(dict->next->data);
 		cpy->next->next = NULL;
