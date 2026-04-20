9156c1d9f5c5d4e060e9b7d352a523946a4d55ff
juperrin <marvin@42.fr>
2026-04-20 14:27:50 +0200

    #norminetted

--- srcs/dictionary/dict_copy.c
+++ srcs/dictionary/dict_copy.c
@@ -6,11 +6,23 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 15:01:29 by juperrin          #+#    #+#             */
-/*   Updated: 2026/04/17 11:35:46 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/20 14:23:12 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static t_dictionary	*copy_next(t_dictionary *dst, const t_dictionary *src)
+{
+	dst->next = dict_new(ft_strdup(src->next->key),
+			ft_strdup((t_string)src->next->data));
+	if (0 == dst->next->key || (0 == dst->next->data && 0 != src->next->data))
+	{
+		perror("malloc");
+		return (NULL);
+	}
+	return (dst);
+}
 
 t_dictionary	*dict_copy(const t_dictionary *dict)
 {
@@ -19,26 +31,21 @@ t_dictionary	*dict_copy(const t_dictionary *dict)
 
 	if (NULL == dict)
 		return (NULL);
-	cpy = malloc(sizeof(t_dictionary));
-	if (NULL == cpy)
+	cpy = dict_new(ft_strdup(dict->key), ft_strdup((t_string)dict->data));
+	if (NULL == cpy->key || (NULL == cpy->data && NULL != dict->data))
 	{
 		perror("malloc");
+		dict_clear(&head);
 		return (NULL);
 	}
 	head = cpy;
-	cpy->key = ft_strdup(dict->key);
-	cpy->data = ft_strdup(dict->data);
 	while (dict->next)
 	{
-		cpy->next = malloc(sizeof(t_dictionary));
-		if (NULL == cpy->next)
+		if (NULL == copy_next(cpy, dict))
 		{
-			perror("malloc");
 			dict_clear(&head);
 			return (NULL);
 		}
-		cpy->next->key = ft_strdup(dict->next->key);
-		cpy->next->data = ft_strdup(dict->next->data);
 		cpy->next->next = NULL;
 		cpy = cpy->next;
 		dict = dict->next;
