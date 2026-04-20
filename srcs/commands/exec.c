1fc38dbc8bef4489ff56ab3dad2c9b72682c5cf5
juperrin <marvin@42.fr>
2026-04-20 18:14:56 +0200

    #norminetted

--- srcs/commands/exec.c
+++ srcs/commands/exec.c
@@ -6,98 +6,59 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 08:48:38 by juperrin          #+#    #+#             */
-/*   Updated: 2026/04/17 11:35:46 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/20 18:13:20 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_status	cmd_exec(t_strings args, t_shell *shell)
-{
-	t_dictionary	*path;
-	t_uint			index;
-	char			**paths;
-	char			**envp;
-	char			*tmp;
+t_status	check_access(t_string *cmd, t_dictionary *env);
 
-	paths = NULL;
-	if (NULL == args || NULL == *args)
-		return (FAILURE);
-	if (SUCCESS != access(*args, F_OK))
+static t_strings	sh_exec_init(const t_strings src)
+{
+	t_strings	args;
+
+	args = malloc(sizeof(t_string) * (strings_len((const t_strings)src) + 2));
+	if (NULL == args)
+		return (NULL);
+	*args = ft_strdup("/bin/sh");
+	if (NULL == *args)
 	{
-		if (str_is_empty(*args))
+		free(args);
+		return (NULL);
+	}
+	return (args);
+}
+
+static t_status	sh_exec(t_strings args, t_strings envp)
+{
+	t_uint		index;
+	t_strings	new_args;
+
+	if (errno != ENOEXEC)
+		return (SUCCESS);
+	new_args = sh_exec_init((const t_strings)args);
+	if (NULL == new_args)
+		return (FAILURE);
+	index = 0;
+	while (*(args + index))
+	{
+		*(new_args + index + 1) = ft_strdup(*(args + index));
+		if (NULL == *(new_args + index + 1))
 		{
-			if (!check_path(shell))
-				error_output(*args, NULL, FILE_NOT_FOUND);
-			else
-				error_output(*args, NULL, COMMAND_NOT_FOUND);
-			return (127);
-		}
-		if (ft_strchr(*args, '/') || !check_path(shell))
-		{
-			error_output(*args, NULL, FILE_NOT_FOUND);
-			return (127);
-		}
-		path = dict_get(shell->env, "PATH");
-		paths = ft_split(path->data, ':');
-		if (NULL == paths)
-		{
-			perror("malloc");
+			strings_free(new_args);
 			return (FAILURE);
 		}
-		index = 0;
-		while (*(paths + index))
-		{
-			tmp = ft_strjoin_sep(*(paths + index), *args, '/');
-			if (NULL == tmp)
-			{
-				perror("malloc");
-				++index;
-				continue ;
-			}
-			if (SUCCESS == access(tmp, F_OK))
-			{
-				free(*args);
-				*args = tmp;
-				break ;
-			}
-			free(tmp);
-			++index;
-		}
-		if (NULL == *(paths + index))
-		{
-			if (ft_strchr(*args, '/') || !check_path(shell))
-				error_output(*args, NULL, FILE_NOT_FOUND);
-			else
-				error_output(*args, NULL, COMMAND_NOT_FOUND);
-			strings_free(paths);
-			return (127);
-		}
+		++index;
 	}
-	if (ft_strchr(*args, '/') && is_dir(*args))
-	{
-		error_output(*args, NULL, IS_DIRECTORY);
-		strings_free(paths);
-		return (126);
-	}
-	envp = dict_to_array(shell->env, '=');
-	execve(*args, args, envp);
-	if (errno == ENOEXEC)
-    {
-        t_strings new_args = malloc(sizeof(t_string ) * (strings_len((const t_strings )args) + 2));
-		*new_args = ft_strdup("/bin/sh");
-		int index = 0;
-		while (*(args + index))
-		{
-			*(new_args + index + 1) = ft_strdup(*(args + index));
-			++index;
-		}
-		*(new_args + index + 1) = NULL;
-        execve(*new_args, new_args, envp);
-		strings_free(new_args);
-	}
-	strings_free(envp);
-	strings_free(paths);
+	*(new_args + index + 1) = NULL;
+	execve(*new_args, new_args, envp);
+	strings_free(new_args);
+	return (SUCCESS);
+}
+
+static t_status	check_perms(t_strings args)
+{
 	if (SUCCESS == access(*args, X_OK) && !is_dir(*args))
 		return (SUCCESS);
 	if (SUCCESS != access(*args, X_OK) && SUCCESS == access(*args, R_OK | W_OK))
@@ -112,4 +73,28 @@ t_status	cmd_exec(t_strings args, t_shell *shell)
 	}
 	error_output(*args, NULL, COMMAND_NOT_FOUND);
 	return (127);
+}
+
+t_status	cmd_exec(t_strings args, t_shell *shell)
+{
+	t_status	status;
+	char		**envp;
+
+	if (NULL == args || NULL == *args)
+		return (FAILURE);
+	status = check_access(args, shell->env);
+	if (SUCCESS != status)
+		return (status);
+	envp = dict_to_array(shell->env, '=');
+	execve(*args, args, envp);
+	status = sh_exec(args, envp);
+	if (SUCCESS != status)
+	{
+		perror("malloc");
+		strings_free(envp);
+		return (status);
+	}
+	strings_free(envp);
+	status = check_perms(args);
+	return (status);
 }
---
+++ srcs/commands/exec_utils.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 18:09:50 by juperrin          #+#    #+#             */
+/*   Updated: 2026/04/20 18:13:14 by juperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static t_status	check_special_cases(t_string cmd, t_dictionary *env)
+{
+	if (str_is_empty(cmd))
+	{
+		if (!check_env_path(env))
+			error_output(cmd, NULL, FILE_NOT_FOUND);
+		else
+			error_output(cmd, NULL, COMMAND_NOT_FOUND);
+		return (FAILURE);
+	}
+	if (ft_strchr(cmd, '/') || !check_env_path(env))
+	{
+		error_output(cmd, NULL, FILE_NOT_FOUND);
+		return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+static t_status	cmd_access(t_string *cmd, t_string path)
+{
+	t_string	tmp;
+
+	if (NULL == path)
+	{
+		perror("malloc");
+		return (FAILURE);
+	}
+	tmp = ft_strjoin_sep(path, *cmd, '/');
+	if (NULL == tmp)
+	{
+		perror("malloc");
+		return (FAILURE);
+	}
+	if (SUCCESS == access(tmp, F_OK))
+	{
+		free(*cmd);
+		*cmd = tmp;
+		return (SUCCESS);
+	}
+	free(tmp);
+	return (FAILURE);
+}
+
+static t_status	get_abs_path(t_string *cmd, t_dictionary *env)
+{
+	const t_dictionary	*path = dict_get(env, "PATH");
+	t_uint				index;
+	t_strings			path_arr;
+
+	path_arr = ft_split(path->data, ':');
+	if (NULL == path_arr)
+	{
+		perror("malloc");
+		return (FAILURE);
+	}
+	index = 0;
+	while (*(path_arr + index))
+	{
+		if (SUCCESS == cmd_access(cmd, ft_strdup(*(path_arr + index))))
+			break ;
+		++index;
+	}
+	if (NULL == *(path_arr + index))
+	{
+		error_output(*cmd, NULL, COMMAND_NOT_FOUND);
+		strings_free(path_arr);
+		return (127);
+	}
+	strings_free(path_arr);
+	return (SUCCESS);
+}
+
+static t_status	check_is_dir(t_string cmd)
+{
+	if (ft_strchr(cmd, '/') && is_dir(cmd))
+	{
+		error_output(cmd, NULL, IS_DIRECTORY);
+		return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+t_status	check_access(t_string *cmd, t_dictionary *env)
+{
+	t_status	status;
+
+	if (SUCCESS != access(*cmd, F_OK))
+	{
+		if (SUCCESS != check_special_cases(*cmd, env))
+			return (127);
+		status = get_abs_path(cmd, env);
+		if (SUCCESS != status)
+			return (status);
+	}
+	if (SUCCESS != check_is_dir(*cmd))
+		return (126);
+	return (SUCCESS);
+}
