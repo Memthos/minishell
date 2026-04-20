fad2aa664ac6ad6182970aa5dcee2a35cd5a605d
julienPR86 <julien.pr86@gmail.com>
2026-04-20 20:45:33 +0200

    feat : splited export function into two files, #norminetted

--- srcs/commands/export.c
+++ srcs/commands/export.c
@@ -6,84 +6,42 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 13:17:30 by juperrin          #+#    #+#             */
-/*   Updated: 2026/04/17 11:35:46 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/20 20:43:07 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+t_status	export_display_env(t_shell *shell);
+t_status	export_special_case(t_strings *args);
+t_strings	export_set_entry(t_strings *args, t_shell *shell);
+t_status	export_check_entry(t_strings *entry, t_strings *args, bool *cat);
+t_status	export_update_env(t_strings *entry, t_shell *shell, bool concat);
+
 t_status	cmd_export(t_strings args, t_shell *shell)
 {
-	bool			concat;
-	t_strings		entry;
-	t_string		tmp;
 	t_status		code;
-	t_dictionary	*cpy;
+	t_strings		entry;
+	bool			concat;
 
-	if (NULL == shell)
-		return (FAILURE);
-	if (NULL == args[1])
-	{
-		cpy = dict_copy(shell->env);
-		dict_sort(&cpy);
-		dict_display(cpy, "declare -x ", "=\"", "\"");
-		dict_clear(&cpy);
-		return (SUCCESS);
-	}
+	if (NULL == *(++args))
+		return (export_display_env(shell));
 	code = SUCCESS;
-	++args;
 	while (*args)
 	{
-		concat = false;
-		if ('=' == **args)
-		{
-			error_output("export", "not a valid identifier", NO_ERR_MSG);
-			code = FAILURE;
-			++args;
+		code = export_special_case(&args);
+		if (SUCCESS != code)
 			continue ;
-		}
-		entry = str_split_at(*args, '=');
+		entry = export_set_entry(&args, shell);
 		if (NULL == entry)
 		{
-			if (!check_var_name(*args))
-			{
-				error_output("export", "not a valid identifier", NO_ERR_MSG);
-				code = FAILURE;
-				++args;
-				continue ;
-			}
-			if (NULL == dict_add(&shell->env, ft_strdup(*args), NULL))
-				perror("malloc");
-			++args;
-			continue ;
-		}
-		if (entry[0][ft_strlen(entry[0]) - 1] == '+')
-		{
-			concat = true;
-			entry[0][ft_strlen(entry[0]) - 1] = '\0';
-		}
-		if (!check_var_name(entry[0]))
-		{
-			error_output("export", "not a valid identifier", NO_ERR_MSG);
-			++args;
-			free(entry[0]);
-			free(entry[1]);
-			free(entry);
 			code = FAILURE;
 			continue ;
 		}
-		if (concat)
-		{
-			cpy = dict_get(shell->env, entry[0]);
-			tmp = ft_strjoin((const t_string)cpy->data, entry[1]);
-			if (NULL == dict_add(&shell->env, entry[0], tmp))
-				perror("malloc");
-			free(entry[1]);
-		}
-		else
-			if (NULL == dict_add(&shell->env, entry[0], entry[1]))
-				perror("malloc");
-		free(entry);
+		code = export_check_entry(&entry, &args, &concat);
+		if (SUCCESS != code)
+			continue ;
+		export_update_env(&entry, shell, concat);
 		++args;
 	}
 	return (code);
---
+++ srcs/commands/export_utils.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 20:41:18 by juperrin          #+#    #+#             */
+/*   Updated: 2026/04/20 20:42:35 by juperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_status	export_display_env(t_shell *shell)
+{
+	t_dictionary	*cpy;
+
+	cpy = dict_copy(shell->env);
+	if (NULL == cpy)
+		return (FAILURE);
+	dict_sort(&cpy);
+	dict_display(cpy, "declare -x ", "=\"", "\"");
+	dict_clear(&cpy);
+	return (SUCCESS);
+}
+
+t_status	export_special_case(t_strings *args)
+{
+	if ('=' == ***args || '+' == ***args)
+	{
+		error_output("export", "not a valid identifier", NO_ERR_MSG);
+		++(*args);
+		return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+t_strings	export_set_entry(t_strings *args, t_shell *shell)
+{
+	const t_dictionary	*cpy = dict_get(shell->env, **args);
+	t_strings			entry;
+
+	entry = str_split_at(**args, '=');
+	if (NULL == entry)
+	{
+		if (!check_var_name(**args))
+		{
+			error_output("export", "not a valid identifier", NO_ERR_MSG);
+			++(*args);
+			return (NULL);
+		}
+		if (!cpy && NULL == dict_add(&shell->env, ft_strdup(**args), NULL))
+			perror("malloc");
+		++(*args);
+	}
+	return (entry);
+}
+
+t_status	export_check_entry(t_strings *entry, t_strings *args, bool *cat)
+{
+	*cat = false;
+	if ((*entry)[0][ft_strlen((*entry)[0]) - 1] == '+')
+	{
+		*cat = true;
+		(*entry)[0][ft_strlen((*entry)[0]) - 1] = '\0';
+	}
+	if (!check_var_name((*entry)[0]))
+	{
+		error_output("export", "not a valid identifier", NO_ERR_MSG);
+		++(*args);
+		free((*entry)[0]);
+		free((*entry)[1]);
+		free((*entry));
+		*entry = NULL;
+		return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+t_status	export_update_env(t_strings *entry, t_shell *shell, bool concat)
+{
+	const t_dictionary	*cpy = dict_get(shell->env, (*entry)[0]);
+	t_string			tmp;
+
+	tmp = (*entry)[1];
+	if (concat && cpy)
+	{
+		tmp = ft_strjoin((const t_string)cpy->data, (*entry)[1]);
+		if (NULL == tmp)
+		{
+			perror("malloc");
+			free((*entry)[0]);
+			free((*entry)[1]);
+			free(*entry);
+			return (FAILURE);
+		}
+		free((*entry)[1]);
+	}
+	if (NULL == dict_add(&shell->env, (*entry)[0], tmp))
+		perror("malloc");
+	free(*entry);
+	return (SUCCESS);
+}
