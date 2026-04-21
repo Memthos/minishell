a0f112f2a52173270da010aa22e07b030adaadbb
juperrin <marvin@42.fr>
2026-04-21 14:30:05 +0200

    # moved utils functions to execute_cmd_utils source file

--- srcs/execute/execute_cmd.c
+++ srcs/execute/execute_cmd.c
@@ -6,120 +6,14 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 14:06:10 by juperrin          #+#    #+#             */
-/*   Updated: 2026/04/21 14:06:30 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/21 14:26:52 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_status	execute_simple_cmd(t_built_in cmd, t_shell *shell)
-{
-	bool	redirected;
-
-	redirected = false;
-	if (-1 != shell->redirects.output_redirect_fd)
-	{
-		redirected = true;
-		redirect_output(&shell->redirects.output_redirect_fd);
-	}
-	else if (-1 != shell->redirects.output_cmp_redirect_fd)
-	{
-		redirected = true;
-		redirect_output(&shell->redirects.output_cmp_redirect_fd);
-	}
-	shell->exitno = cmd(shell->cur_cmd, shell);
-	if (redirected && dup2(shell->redirects.stdout_dup, STDOUT_FILENO) < 0)
-	{
-		perror("dup2");
-		shell->exitno = FAILURE;
-		return (shell->exitno);
-	}
-	return (shell->exitno);
-}
-
-static t_status	execute_cmd_child_pipe(t_shell *shell)
-{
-	t_pipe	*input_pipe;
-	t_pipe	*output_pipe;
-
-	input_pipe = pipe_get(shell, RIGHT);
-	output_pipe = pipe_get(shell, LEFT);
-	if (input_pipe && shell->redirect_input)
-	{
-		if (SUCCESS != redirect_input(&input_pipe->pipe[0]))
-		{
-			destroy_shell(shell);
-			exit(FAILURE);
-		}
-	}
-	if (output_pipe && shell->redirect_output)
-	{
-		if (SUCCESS != redirect_output(&output_pipe->pipe[1]))
-		{
-			destroy_shell(shell);
-			exit(FAILURE);
-		}
-	}
-	stack_lst_clear(&shell->pipe_stack, (void (*)(void *)) & pipe_close);
-	return (SUCCESS);
-}
-
-static t_status	execute_cmd_child_input_redirection(t_shell *shell)
-{
-	if (-1 != shell->redirects.input_redirect_fd)
-	{
-		if (SUCCESS != redirect_input(&shell->redirects.input_redirect_fd))
-		{
-			destroy_shell(shell);
-			exit(FAILURE);
-		}
-	}
-	else if (-1 != shell->redirects.input_cmp_redirect_fd)
-	{
-		if (SUCCESS != redirect_input(&shell->redirects.input_cmp_redirect_fd))
-		{
-			destroy_shell(shell);
-			exit(FAILURE);
-		}
-	}
-	return (SUCCESS);
-}
-
-static t_status	execute_cmd_child_output_redirection(t_shell *shell)
-{
-	if (-1 != shell->redirects.output_redirect_fd)
-	{
-		if (SUCCESS != redirect_output(&shell->redirects.output_redirect_fd))
-		{
-			destroy_shell(shell);
-			exit(FAILURE);
-		}
-	}
-	else if (-1 != shell->redirects.output_cmp_redirect_fd)
-	{
-		if (SUCCESS != redirect_output(&shell->redirects.output_cmp_redirect_fd))
-		{
-			destroy_shell(shell);
-			exit(FAILURE);
-		}
-	}
-	return (SUCCESS);
-}
-
-static t_status	execute_cmd_child(t_built_in cmd, t_shell *shell)
-{
-	t_status	status;
-	
-	restore_signals();
-	ft_close(&shell->redirects.stdin_dup);
-	ft_close(&shell->redirects.stdout_dup);
-	execute_cmd_child_pipe(shell);
-	execute_cmd_child_input_redirection(shell);
-	execute_cmd_child_output_redirection(shell);
-	status = cmd(shell->cur_cmd, shell);
-	destroy_shell(shell);
-	exit(status);
-}
+t_status	execute_simple_cmd(t_built_in cmd, t_shell *shell);
+t_status	execute_cmd_child(t_built_in cmd, t_shell *shell);
 
 t_status	execute_cmd(t_shell *shell)
 {
