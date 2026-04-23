e847e60f3a62934aa3fddcd022f3e0e926133249
julienPR86 <marvin@42.fr>
2026-04-23 12:50:42 +0200

    + added somme comments

--- includes/pipe.h
+++ includes/pipe.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 15:15:39 by juperrin          #+#    #+#             */
-/*   Updated: 2026/04/09 17:03:09 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/23 12:50:11 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,18 @@ typedef struct s_pipe
 	int		pipe[2];	
 }	t_pipe;
 
+/**
+ * @brief Creates an allocated t_pipe struct.
+ */
 t_pipe	*pipe_new(void);
 
+/**
+ * @brief Closes open fds in pipe and free it.
+ */
 void	pipe_close(t_pipe *pipe);
 
+/**
+ * @brief Returns the last pipe of the shell's pipe stack
+ * with the corresponding side.
+ */
 t_pipe	*pipe_get(t_shell *shell, t_side side);
