61e72cf755df99c9efd05879a4afa93183a2df5c
julienPR86 <julien.pr86@gmail.com>
2026-04-23 11:14:08 +0200

    fix : export now returns failure in case of one wrong argument instead of returning the error code of the last entry creation

--- srcs/commands/export.c
+++ srcs/commands/export.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 13:17:30 by juperrin          #+#    #+#             */
-/*   Updated: 2026/04/21 22:17:11 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/23 11:12:30 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,37 @@ t_strings	export_set_entry(t_strings *args);
 t_status	export_check_entry(t_strings *entry, t_strings *args, bool *cat);
 t_status	export_update_env(t_strings *entry, t_shell *shell, bool concat);
 
+static t_status	export_logic(t_strings *args, t_shell *shell)
+{
+	bool		concat;
+	t_strings	entry;
+
+	if (SUCCESS != export_special_case(args))
+		return (FAILURE);
+	entry = export_set_entry(args);
+	if (NULL == entry)
+		return (FAILURE);
+	if (SUCCESS != export_check_entry(&entry, args, &concat))
+		return (FAILURE);
+	if (SUCCESS != export_update_env(&entry, shell, concat))
+		return (FAILURE);
+	return (SUCCESS);
+}
+
 t_status	cmd_export(t_strings args, t_shell *shell)
 {
-	t_status		code;
-	t_strings		entry;
-	bool			concat;
+	t_status	code;
 
 	if (NULL == *(++args))
 		return (export_display_env(shell));
 	code = SUCCESS;
 	while (*args)
 	{
-		code = export_special_case(&args);
-		if (SUCCESS != code)
-			continue ;
-		entry = export_set_entry(&args);
-		if (NULL == entry)
+		if (SUCCESS != export_logic(&args, shell))
 		{
 			code = FAILURE;
 			continue ;
 		}
-		code = export_check_entry(&entry, &args, &concat);
-		if (SUCCESS != code)
-			continue ;
-		export_update_env(&entry, shell, concat);
 		++args;
 	}
 	return (code);
