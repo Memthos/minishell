04c74f93b58a81486e9183f9a1c7a740f2f37336
julienPR86 <marvin@42.fr>
2026-04-25 16:08:23 +0200

    + added comments

--- includes/redirection.h
+++ includes/redirection.h
@@ -6,12 +6,18 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 21:08:08 by juperrin          #+#    #+#             */
-/*   Updated: 2026/04/20 21:22:06 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/04/25 16:08:15 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "types.h"
 
+/**
+ * @brief Redirects standard input to fd.
+ */
 t_status	redirect_input(int *fd);
 
+/**
+ * @brief Redirects standard output to fd.
+ */
 t_status	redirect_output(int *fd);
